bdc4ed63834b1718a3c33638fc44f345418909a1
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2024-02-08 17:10:27 -0300

    listas

---
+++ 10-listas.py
@@ -0,0 +1,21 @@
+'''Nessa lista ja vai conter todas as informaçoes das outras variáveis mas em uma só,
+se não estivesse usando essa estrutura, teria que criar váriaveis diferentes para cada informação
+'''
+
+gameFifa = ["Fifa 24", 2023, 140.0, False]
+print(gameFifa)
+
+gamesList = ["Palworld", "Spider-Man 2", "The Last of Us", "The Legend  of Zelda"]
+print(gamesList)
+
+# 1 - Buscar os dois primeiros itens da lista
+print(gamesList[0:2])
+
+# 2 - Buscar o último item da lista
+print(gamesList[-1])
+
+# 3 - Buscar jogos até uma determinada posição
+print(gamesList[:3])
+
+# 4 - Buscar jogos de uma posição em diante
+print(gamesList[1:3])
